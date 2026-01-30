ac7498221799a29cf07bcad06ff6c9698b32af14
rhuanaa <rhuanandrey@gmail.com>
2026-01-30 15:42:32 -0300

    feat: maoDeOnze database added

---
+++ domain/src/main/java/com/bueno/domain/usecases/hand/repos/MaoDeOnzeRepository.java
@@ -0,0 +1,7 @@
+package com.bueno.domain.usecases.hand.repos;
+
+import com.bueno.domain.usecases.hand.dtos.MaoDeOnzeDto;
+
+public interface MaoDeOnzeRepository {
+    void save(MaoDeOnzeDto dto);
+}
