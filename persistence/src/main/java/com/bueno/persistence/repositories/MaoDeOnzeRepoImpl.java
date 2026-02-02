5a0e6e1c1fa9882776ad09a99c97390c49ecfd44
rhuanaa <rhuanandrey@gmail.com>
2026-02-02 15:26:40 -0300

    fix: hand_winner added

--- persistence/src/main/java/com/bueno/persistence/repositories/MaoDeOnzeRepoImpl.java
+++ persistence/src/main/java/com/bueno/persistence/repositories/MaoDeOnzeRepoImpl.java
@@ -14,8 +14,9 @@ public class MaoDeOnzeRepoImpl implements MaoDeOnzeRepository {
     @Override
     public void save(MaoDeOnzeDto dto) {
         String sql = """
-                INSERT INTO mao_de_onze(
+            INSERT INTO mao_de_onze(
                 uuid,
+                game_uuid,
                 weak_card,
                 medium_card,
                 strong_card,
@@ -24,23 +25,24 @@ public class MaoDeOnzeRepoImpl implements MaoDeOnzeRepository {
                 opponent_points,
                 open_hand,
                 hand_winner
-                )
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
-                """;
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+            """;
         try (PreparedStatement preparedStatement = ConnectionFactory.createPreparedStatement(sql)) {
             preparedStatement.setObject(1, UUID.randomUUID());
-            preparedStatement.setInt(2, dto.weakCardValue());
-            preparedStatement.setInt(3, dto.mediumCardValue());
-            preparedStatement.setInt(4, dto.strongCardValue());
-            preparedStatement.setString(5, dto.player_type());
-            preparedStatement.setInt(6, dto.player_points());
-            preparedStatement.setInt(7, dto.opponents_points());
-            preparedStatement.setBoolean(8, dto.openHand());
-            preparedStatement.setBoolean(9, dto.handWinner());
+            preparedStatement.setObject(2, dto.gameUuid());
+            preparedStatement.setInt(3, dto.weakCardValue());
+            preparedStatement.setInt(4, dto.mediumCardValue());
+            preparedStatement.setInt(5, dto.strongCardValue());
+            preparedStatement.setString(6, dto.player_type());
+            preparedStatement.setInt(7, dto.player_points());
+            preparedStatement.setInt(8, dto.opponents_points());
+            preparedStatement.setBoolean(9, dto.openHand());
+            preparedStatement.setBoolean(10, dto.handWinner());
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
-            System.err.println(e.getClass() + ": " + e.getMessage() + "| HandResult couldn't be saved");
+            System.err.println(e.getClass() + ": " + e.getMessage() + "| MaoDeOnze couldn't be saved");
             e.printStackTrace();
         }
     }
