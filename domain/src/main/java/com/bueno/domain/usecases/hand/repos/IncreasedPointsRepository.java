f458d16e6640be29a0b23a0253a9765d3ceb93d5
rhuanaa <rhuanandrey@gmail.com>
2026-02-18 22:19:36 -0300

    feat: find all and find by game to IncreasedPoints

--- domain/src/main/java/com/bueno/domain/usecases/hand/repos/IncreasedPointsRepository.java
+++ domain/src/main/java/com/bueno/domain/usecases/hand/repos/IncreasedPointsRepository.java
@@ -2,6 +2,9 @@ package com.bueno.domain.usecases.hand.repos;
 
 import com.bueno.domain.usecases.hand.dtos.IncreasedPointsDto;
 
-public interface IncreasedPointsRepository {
-    void save(IncreasedPointsDto dto);
-}
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IncreasedPointsRepository
+        extends DatasetRepository <IncreasedPointsDto>
+{}
