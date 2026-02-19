e9ca4982904de3449731b2623b5694abc6a844ce
rhuanaa <rhuanandrey@gmail.com>
2026-02-18 22:19:36 -0300

    feat: find all and find by game to IncreasedPoints

---
+++ domain/src/main/java/com/bueno/domain/usecases/hand/repos/DatasetRepository.java
@@ -0,0 +1,10 @@
+package com.bueno.domain.usecases.hand.repos;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DatasetRepository <T> {
+    void save(T data);
+    List<T> findByGameUuid(UUID gameUuid);
+    List<T> findAll();
+}
--- persistence/src/main/java/com/bueno/persistence/repositories/IncreasedPointsRepoImpl.java
+++ persistence/src/main/java/com/bueno/persistence/repositories/IncreasedPointsRepoImpl.java
@@ -5,9 +5,11 @@ import com.bueno.domain.usecases.hand.repos.IncreasedPointsRepository;
 import com.bueno.persistence.ConnectionFactory;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Array;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class IncreasedPointsRepoImpl implements IncreasedPointsRepository {
@@ -60,5 +62,70 @@ public class IncreasedPointsRepoImpl implements IncreasedPointsRepository {
             System.err.println(e.getClass() + ": " + e.getMessage() + "| IncreasePoints couldn't be saved");
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public List<IncreasedPointsDto> findByGameUuid(UUID gameUuid) {
+        String sql = "SELECT * FROM increased_points WHERE game_uuid = ?";
+        List<IncreasedPointsDto> results = new ArrayList<>();
+
+        try (PreparedStatement statement = ConnectionFactory.createPreparedStatement(sql)) {
+            statement.setObject(1, gameUuid);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    results.add(mapResultSetToDto(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass() + ": " + e.getMessage() + "| Could not find IncreasedPoints by Game UUID");
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    @Override
+    public List<IncreasedPointsDto> findAll() {
+        String sql = "SELECT * FROM increased_points";
+        List<IncreasedPointsDto> results = new ArrayList<>();
+
+        try (PreparedStatement statement = ConnectionFactory.createPreparedStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                results.add(mapResultSetToDto(resultSet));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass() + ": " + e.getMessage() + "| Could not retrieve all IncreasedPoints");
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    private IncreasedPointsDto mapResultSetToDto(ResultSet rs) throws SQLException {
+        Array sqlArray = rs.getArray("pile");
+        List<Integer> pileList = new ArrayList<>();
+        if (sqlArray != null) {
+            Integer[] javaArray = (Integer[]) sqlArray.getArray();
+            pileList.addAll(Arrays.asList(javaArray));
+        }
+
+        return new IncreasedPointsDto(
+                UUID.fromString(rs.getString("game_uuid")),
+                rs.getInt("weak_card"),
+                rs.getInt("medium_card"),
+                rs.getInt("strong_card"),
+                rs.getString("player_type"),
+                rs.getBoolean("open_hand"),
+                pileList,
+                rs.getDouble("winner_r1"),
+                rs.getDouble("winner_r2"),
+                rs.getDouble("winner_r3"),
+                rs.getInt("player_points"),
+                rs.getInt("opponent_points"),
+                rs.getInt("hand_value"),
+                rs.getInt("opponent_accepted"),
+                rs.getInt("general_score_impact")
+        );
     }
 }
