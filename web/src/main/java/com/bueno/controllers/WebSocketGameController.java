feecbf28a06306b5976bb8ffae0ef6ff36c3b794
rhuanaa <rhuanandrey@gmail.com>
2026-02-26 09:12:07 -0300

    fix: double delete game

--- web/src/main/java/com/bueno/controllers/WebSocketGameController.java
+++ web/src/main/java/com/bueno/controllers/WebSocketGameController.java
@@ -1,5 +1,6 @@
 package com.bueno.controllers;
 
+import com.bueno.domain.usecases.game.repos.GameRepository;
 import com.bueno.domain.usecases.game.usecase.RemoveGameUseCase;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,16 +15,20 @@ public class WebSocketGameController {
 
     private final RemoveGameUseCase removeGameUseCase;
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final GameRepository gameRepository;
 
 
-    public WebSocketGameController(RemoveGameUseCase removeGameUseCase, SimpMessagingTemplate simpMessagingTemplate) {
+    public WebSocketGameController(RemoveGameUseCase removeGameUseCase, SimpMessagingTemplate simpMessagingTemplate, GameRepository gameRepository) {
         this.removeGameUseCase = removeGameUseCase;
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.gameRepository = gameRepository;
     }
 
     @MessageMapping("/games/{gameUuid}/quit")
     public void quitGame(@DestinationVariable UUID gameUuid, UUID playerUuid) {
-        removeGameUseCase.byUserUuid(playerUuid, true);
+        if (gameRepository.findByPlayerUuid(playerUuid).isPresent()) {
+            removeGameUseCase.byUserUuid(playerUuid, true);
+        }
         simpMessagingTemplate.convertAndSend("/topic/game/" + gameUuid + "/quit", gameUuid);
     }
 }
